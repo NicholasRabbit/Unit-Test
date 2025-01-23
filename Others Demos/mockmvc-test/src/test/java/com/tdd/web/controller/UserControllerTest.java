861544246564497534e6aac414cdc183ec2b3b9d
plms <wuyangzerg@gmail.com>
2025-01-23 13:59:30 +0800

    udpate

--- Others Demos/mockmvc-test/src/test/java/com/tdd/web/controller/UserControllerTest.java
+++ Others Demos/mockmvc-test/src/test/java/com/tdd/web/controller/UserControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,8 +37,20 @@ public class UserControllerTest {
 
     @Test
     public void getByName() throws Exception {
-        mockMvc.perform(get("/user/getByName")
-                .param("name", "Tom"));
+        MvcResult mvcResult = mockMvc.perform(get("/user/getByName")
+                        .param("name", "Tom"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String jsonString = response.getContentAsString();
+        JSONObject jsonObj = new JSONObject(jsonString);
+
+        JSONObject entries = JSONUtil.parseObj(jsonObj.get("data"));
+        assertEquals(entries.get("bucketName"), "e-task");
+        assertEquals(entries.get("fileName"), "WX_GQ_0000000757-2024-1003.pdf");
+        assertEquals(entries.get("url"), String.format("/admin/sys-file/%s/%s", "e-task", "WX_GQ_0000000757-2024-1003.pdf"));
+
     }
 
 }
---
+++ git commands.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+git status
+
+git add --all
+
+: : Just directly write the committing message without any quoations marks.
+set /p message=Enter message for Command 1: 
+
+git commit -m "%message%"
+
+git push origin master
+
+git push origin-github master
+git push origin-gitee master
+
+PAUSE
+
